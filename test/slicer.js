9b4e52fb3acf0cfa675ea825837ce3efac39f5c0
Dan Bornstein <danfuzz@obvious.com>
2012-06-29 13:19:22 -0700

    Add a readable-transition test.
    This demonstrates that, as I expected, I didn't get the semantics right
    when the upstream source is ended but the buffered data isn't yet
    drained.

--- test/slicer.js
+++ test/slicer.js
@@ -11,6 +11,9 @@ var events = require("events");
 
 var Slicer = require("../").Slicer;
 
+var CallbackCollector = require("./cbcoll").CallbackCollector;
+var emit = require("./emit").emit;
+
 
 /*
  * Tests
@@ -44,10 +47,53 @@ function constructorFailures() {
   assert.throws(f3, /Invalid encoding name/);
 }
 
+/**
+ * Test the transition from readable to un-readable.
+ */
+function readableTransition() {
+  var theData = new Buffer("Scones are delicious.");
+
+  tryWith(false, "end");
+  tryWith(false, "close");
+  tryWith(false, "close", new Error("oy"));
+  tryWith(false, "error", new Error("oof"));
+  tryWith(true, "end");
+  tryWith(true, "close");
+  tryWith(true, "close", new Error("oy"));
+  tryWith(true, "error", new Error("oof"));
+
+  function tryWith(doData, endEvent, endArg) {
+    var source = new events.EventEmitter();
+    var slicer = new Slicer(source);
+    var coll = new CallbackCollector();
+
+    assert.ok(slicer.readable);
+
+    if (doData) {
+      source.emit("data", theData);
+      assert.ok(slicer.readable);
+    }
+
+    if (endEvent) {
+      emit(source, endEvent, endArg);
+    }
+
+    if (doData) {
+      assert.ok(slicer.readable);
+      slicer.readAll(coll.callback);
+      assert.equal(coll.callbacks.length, 1);
+      coll.assertCallback(0, undefined, theData.length, theData, 0);
+    }
+
+    assert.ok(!slicer.readable);
+  }
+}
+
 
 function test() {
   constructor();
   constructorFailures();
+  readableTransition();
 }
 
 module.exports = {
