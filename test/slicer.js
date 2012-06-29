393fac5f2e0789d2060bc427c0fd1019f9dcf178
Dan Bornstein <danfuzz@obvious.com>
2012-06-29 15:48:15 -0700

    Add a test to make sure callbacks don't get unintentionally reused.

--- test/slicer.js
+++ test/slicer.js
@@ -532,6 +532,36 @@ function setIncomingEncoding() {
   }
 }
 
+/**
+ * Make sure callbacks aren't getting unintentionally reused. (The
+ * other multi-callback test cases end up intentionally reusing
+ * callbacks, for programmer convenience.)
+ */
+function noCallbackReuse() {
+  var theData = new Buffer("cannelle");
+
+  var source = new events.EventEmitter();
+  var slicer = new Slicer(source);
+  var colls = [];
+
+  for (var i = 0; i < 20; i++) {
+    colls[i] = new CallbackCollector();
+    slicer.read(1, colls[i].callback);
+  }
+
+  source.emit("data", theData);
+  source.emit("end");
+
+  for (i = 0; i < colls.length; i++) {
+    var one = colls[i];
+    var expectBuf =
+      (i < theData.length) ? theData.slice(i, i + 1) : new Buffer(0);
+
+    assert.equal(one.callbacks.length, 1);
+    one.assertCallback(0, false, expectBuf.length, expectBuf, 0);
+  }
+}
+
 function test() {
   constructor();
   constructorFailures();
@@ -547,6 +577,7 @@ function test() {
   partialRead();
   constructorEncodings();
   setIncomingEncoding();
+  noCallbackReuse();
 }
 
 module.exports = {
